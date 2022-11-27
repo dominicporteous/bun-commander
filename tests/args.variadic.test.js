cb52157819d6b74aa4a3dd3c19a2d3f94b99d855
Dom <dominic@dombox.xyz>
2022-11-27 22:56:04 +0000

    test: args.varadic.test.js

--- tests/args.variadic.test.js
+++ tests/args.variadic.test.js
@@ -1,11 +1,13 @@
 import { describe, expect, it } from "bun:test";
+import { throws } from 'node:assert'
 import commander from '../index.js';
+import sinon from 'sinon'
 
 // Testing variadic arguments. Testing all the action arguments, but could test just variadicArg.
 
 describe('variadic argument', () => {
   it('when no extra arguments specified for program then variadic arg is empty array', () => {
-    const actionMock = jest.fn();
+    const actionMock = sinon.spy();
     const program = new commander.Command();
     program
       .arguments('<id> [variadicArg...]')
@@ -13,11 +15,11 @@ describe('variadic argument', () => {
 
     program.parse(['node', 'test', 'id']);
 
-    expect(actionMock).toHaveBeenCalledWith('id', [], program.opts(), program);
+    expect(actionMock.lastCall.args).toEqual(['id', [], program.opts(), program]);
   });
 
   it('when extra arguments specified for program then variadic arg is array of values', () => {
-    const actionMock = jest.fn();
+    const actionMock = sinon.spy();
     const program = new commander.Command();
     program
       .arguments('<id> [variadicArg...]')
@@ -26,11 +28,11 @@ describe('variadic argument', () => {
 
     program.parse(['node', 'test', 'id', ...extraArguments]);
 
-    expect(actionMock).toHaveBeenCalledWith('id', extraArguments, program.opts(), program);
+    expect(actionMock.lastCall.args).toEqual(['id', extraArguments, program.opts(), program]);
   });
 
   it('when no extra arguments specified for command then variadic arg is empty array', () => {
-    const actionMock = jest.fn();
+    const actionMock = sinon.spy();
     const program = new commander.Command();
     const cmd = program
       .command('sub [variadicArg...]')
@@ -38,11 +40,11 @@ describe('variadic argument', () => {
 
     program.parse(['node', 'test', 'sub']);
 
-    expect(actionMock).toHaveBeenCalledWith([], cmd.opts(), cmd);
+    expect(actionMock.lastCall.args).toEqual([[], cmd.opts(), cmd]);
   });
 
   it('when extra arguments specified for command then variadic arg is array of values', () => {
-    const actionMock = jest.fn();
+    const actionMock = sinon.spy();
     const program = new commander.Command();
     const cmd = program
       .command('sub [variadicArg...]')
@@ -51,23 +53,23 @@ describe('variadic argument', () => {
 
     program.parse(['node', 'test', 'sub', ...extraArguments]);
 
-    expect(actionMock).toHaveBeenCalledWith(extraArguments, cmd.opts(), cmd);
+    expect(actionMock.lastCall.args).toEqual([extraArguments, cmd.opts(), cmd]);
   });
 
   it('when program variadic argument not last then error', () => {
     const program = new commander.Command();
 
-    expect(() => {
+    throws(() => {
       program.arguments('<variadicArg...> [optionalArg]');
-    }).toThrow("only the last argument can be variadic 'variadicArg'");
+    }, { message: "only the last argument can be variadic 'variadicArg'" });
   });
 
   it('when command variadic argument not last then error', () => {
     const program = new commander.Command();
 
-    expect(() => {
+    throws(() => {
       program.command('sub <variadicArg...> [optionalArg]');
-    }).toThrow("only the last argument can be variadic 'variadicArg'");
+    }, { message: "only the last argument can be variadic 'variadicArg'" });
   });
 
   it('when variadic argument then usage shows variadic', () => {
